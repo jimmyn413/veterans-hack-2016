b5b0a7d839d0d44a7ed8f005c01cbce54c9fbc3c
Yushi Gomi <yushigomi@hotmail.com>
2016-09-03 11:56:05 -0700

    userprofileapi  conflict

--- Sabio.Web/Controllers/Api/UserProfileApiController.cs
+++ Sabio.Web/Controllers/Api/UserProfileApiController.cs
@@ -1,7 +1,4 @@
-﻿using Sabio.Web.Domain;
-using Sabio.Web.Models.Responses;
-using Sabio.Web.Services;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -10,24 +7,9 @@ using System.Web.Http;
 
 namespace Sabio.Web.Controllers.Api
 {
-    [RoutePrefix("api/user")]
     public class UserProfileApiController : ApiController
     {
-        [Route, HttpGet]
-        public HttpResponseMessage GetUser(string userId)
-        {
 
-
-            //senderId = UserService.GetCurrentUserId();
-
-            UserProfile user = UserProfileService.GetUser(userId);
-
-            ItemResponse<UserProfile> response = new ItemResponse<UserProfile>();
-
-            response.Item = user;
-
-            return Request.CreateResponse(HttpStatusCode.OK, response);
-        }
 
     }
 }
