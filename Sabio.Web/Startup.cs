2ed4ed8b97be65e09b476b3d0976b864931ee5e1
glitch <glitch@DESKTOP-1JQ2A1R>
2016-09-02 19:55:56 -0700

    Add project files.

---
+++ Sabio.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sabio.Web.Startup))]
+namespace Sabio.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
