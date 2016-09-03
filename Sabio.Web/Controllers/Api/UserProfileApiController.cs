ea5acfaf5ef3bb66de32c99d1d329cc5688ad505
Yushi Gomi <yushigomi@hotmail.com>
2016-09-03 12:06:08 -0700

    userprofileapi back to jimmys

--- Sabio.Web/Controllers/Api/UserProfileApiController.cs
+++ Sabio.Web/Controllers/Api/UserProfileApiController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Sabio.Web.Domain;
+using Sabio.Web.Models.Responses;
+using Sabio.Web.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,9 +10,24 @@ using System.Web.Http;
 
 namespace Sabio.Web.Controllers.Api
 {
+    [RoutePrefix("api/user")]
     public class UserProfileApiController : ApiController
     {
+        [Route, HttpGet]
+        public HttpResponseMessage GetUser(string userId)
+        {
 
+
+            //senderId = UserService.GetCurrentUserId();
+
+            UserProfile user = UserProfileService.GetUser(userId);
+
+            ItemResponse<UserProfile> response = new ItemResponse<UserProfile>();
+
+            response.Item = user;
+
+            return Request.CreateResponse(HttpStatusCode.OK, response);
+        }
 
     }
 }
