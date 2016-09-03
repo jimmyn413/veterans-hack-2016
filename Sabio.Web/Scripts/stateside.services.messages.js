be7e3f146bf7d6d63627c6c9617e2d43b82c18b7
Jimmy <Jimmy@DESKTOP-28MVTHI>
2016-09-03 08:11:35 -0700

    onemorenamechange

--- Sabio.Web/Scripts/stateside.services.messages.js
+++ Sabio.Web/Scripts/stateside.services.messages.js
@@ -1,4 +1,4 @@
-﻿sabio.services.messages = function (senderId, receiverId, onSuccess, onError) {
+﻿stateside.services.messages = function (senderId, receiverId, onSuccess, onError) {
     var url = "/api/messages/?senderId=" + senderId + "&receiverId=" + receiverId;
 
     var settings = {
