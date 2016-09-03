1cc719f2fe617d2825d4eae1ce9990c8166f8c09
Yushi Gomi <yushigomi@hotmail.com>
2016-09-03 07:44:01 -0700

    ajax get message

--- Sabio.Web/Scripts/sabio.services.messages.js
+++ Sabio.Web/Scripts/sabio.services.messages.js
@@ -1,10 +1,10 @@
-﻿sabio.services.messages = function (onSuccess, onError) {
-    var url = "/api/messages/";
+﻿sabio.services.messages = function (senderId, receiverId, onSuccess, onError) {
+    var url = "/api/messages/?senderId=" + senderId + "&receiverId=" + receiverId;
 
     var settings = {
         cache: false
-        , contentType: "application/x-www-form-urlencoded; charset=UTF-8"
-        , data: data
+        //, contentType: "application/x-www-form-urlencoded; charset=UTF-8"
+        //, data: data
         , dataType: "json"
         , success: onSuccess
         , error: onError
