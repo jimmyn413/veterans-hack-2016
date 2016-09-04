ce1f1bdb818916baa8442376d5a6bf05676b848f
Varr Willis <varr.willis@gmail.com>
2016-09-03 21:07:05 -0700

    Added methods to the Event Service and created AttendeeRequest model

--- Sabio.Web/Services/EventService.cs
+++ Sabio.Web/Services/EventService.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Sabio.Web.Domain;
+using Sabio.Web.Models.Requests;
 
 namespace Sabio.Web.Services
 {
     public class EventService
     {
 
-        //- GetEventDetail(int Id)
+        public static Event GetEventDetail(int Id)
+        {
+            Event ThisEvent = new Event();
+
+            return ThisEvent;
+        }
 
         public static List<Event> GetAllEvents()
         {
@@ -28,6 +34,28 @@ namespace Sabio.Web.Services
             return EventList;
         }
 
+
+        public static List<UserProfile> GetAllAttendees(int EventId)
+        {
+            List<UserProfile> AttendeeList = new List<UserProfile>();
+
+            return AttendeeList;
+        }
+
+
+        public static void AddEventAttendee(int EventId, string UserId)
+        {
+            //- Status should default to Pending (1)
+        }
+
+        //- Attendee Status
+        //- 1 = Pending
+        //- 2 = Accepted
+        //- 3 = Declined
+        public static void UpdateAttendeeStatus(int EventId, string UserId)
+        {
+
+        }
         
 
     }
